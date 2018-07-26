f16fb40f762f6be2054889511dc74c5697870ce3
Tanner Helton <tannerwh@gmail.com>
2018-07-26 15:55:40 -0500

    Header text as prop

--- albums/index.js
+++ albums/index.js
@@ -1,7 +1,12 @@
-/** @format */
-
+// Import a library
+import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import Header from './src/components/header'
 
-AppRegistry.registerComponent(appName, () => App);
+// Create a component
+const App = () => (
+    <Header headerText={'Albums'} />
+);
+
+// Render the component
+AppRegistry.registerComponent('albums', () => App);
