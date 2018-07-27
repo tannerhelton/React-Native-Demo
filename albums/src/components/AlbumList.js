af9342dffc5122ed0a4097a46790159afb7189de
Tanner Helton <tannerwh@gmail.com>
2018-07-27 15:51:03 -0500

    Cards

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -5,17 +5,17 @@ import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
     state = {albums: []};
-    
+
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => this.setState({albums: response.data}));
     }
 
     renderAlbums() {
-        return this.state.albums.map(album => 
+        return this.state.albums.map(album =>
             <AlbumDetail album={album} key={album.title} />
         );
     }
-    
+
     render() {
         console.log(this.state);
         return (
