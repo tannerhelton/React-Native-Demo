015dac0ae9f7ccb131f62006bf119fdcb8be04c4
Tanner Helton <tannerwh@gmail.com>
2018-07-29 11:12:25 -0500

    Butto done

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, Linking} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
@@ -9,7 +9,8 @@ const AlbumDetail = ( { album } ) => {
     image,
     title,
     artist,
-    thumbnail_image
+    thumbnail_image,
+    url
   } = album;
 
   const {
@@ -42,7 +43,7 @@ const AlbumDetail = ( { album } ) => {
           </CardSection>
 
           <CardSection>
-            <Button />
+            <Button onPress={ () => Linking.openURL(url) } />
           </CardSection>
         </Card>
     );
