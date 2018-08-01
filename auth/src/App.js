f95afb3159dc53952d6a4c4064193e9410ae7269
Tanner Helton <tannerwh@gmail.com>
2018-08-01 01:06:24 -0500

    Connect firebase

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common'
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAklgWqOKNkEHHbYVsySQPUwFdNdoKp8LQ",
+      authDomain: "auth-786c1.firebaseapp.com",
+      databaseURL: "https://auth-786c1.firebaseio.com",
+      projectId: "auth-786c1",
+      storageBucket: "auth-786c1.appspot.com",
+      messagingSenderId: "1069418813207"
+  })
+  }
+
   render () {
     return (
       <View>
-
+      <Header headerText="Authentication" />
+        <Text>An App!</Text>
       </View>
     );
   }
 }
+
+export default App
